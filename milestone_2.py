02d9d7b11368efbd3730917a137b186c1620a318
Niall <niallcanning@icloud.com>
2023-11-02 18:16:23 +0000

    user input + game setup

---
+++ milestone_2.py
@@ -0,0 +1,14 @@
+import random
+
+word_list = ["apple", "kiwi", "pineapple", "blueberry", "bannana"]
+word = random.choice(word_list)
+
+guess = input("Enter a single letter: ")
+
+if (len(guess) == 1) and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
+
+# print(word_list)
+# print(word)
